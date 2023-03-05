4e36fab3c36381ab1c169b5fc4e9d9f0408eddc2
DJHoon24 <djhoon24@gmail.com>
2023-03-05 16:38:20 -0500

    Balanced Binary Tree Solution

---
+++ LC_Easy/LC110_balancedBinaryTree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return self.getHeight(root) != -1
+        
+    
+    def getHeight(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        leftHeight = self.getHeight(root.left)
+        rightHeight = self.getHeight(root.right)
+        if leftHeight == -1 or rightHeight == -1 or (abs(leftHeight - rightHeight) > 1):
+            return -1
+        return max(leftHeight + 1, rightHeight + 1)
