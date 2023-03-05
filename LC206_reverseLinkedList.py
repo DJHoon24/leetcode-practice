c8168f7d705fbb5f3a59cead9b79d99e660c1f1a
DJHoon24 <djhoon24@gmail.com>
2023-03-05 15:52:25 -0500

    Reverse Linked list solution

---
+++ LC206_reverseLinkedList.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curnode = head
+        prevnode = None
+        while curnode:
+            tmpnode = curnode.next
+            curnode.next = prevnode
+            prevnode = curnode
+            curnode = tmpnode
+        return prevnode
