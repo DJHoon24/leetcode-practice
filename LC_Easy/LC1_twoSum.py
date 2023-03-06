10b3b3b695161ff20228d4c683d6db234201de36
DJHoon24 <djhoon24@gmail.com>
2023-03-06 10:06:57 -0500

    Fix bugs in solutions

--- LC_Easy/LC1_twoSum.py
+++ LC_Easy/LC1_twoSum.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         possibleTargets = {}
@@ -6,3 +8,7 @@ class Solution:
                 return [possibleTargets[num], i]
             possibleTargets[target-num] = i
         return [-1, -1]
+
+solution = Solution()
+print(solution.twoSum([1,2,3,4,5], 5))
+
